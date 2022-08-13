163fc5aa3078b837c8e3b67ec1697296afb6a1c7
Stella Ling <stellalph2@gmail.com>
2022-08-14 09:16:39 +0930

    remove unwanted images and add new images

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -8,8 +8,8 @@ db.once('open', async () => {
     { name: 'Family' },
     { name: 'Individual' },
     { name: 'Group' },
-    { name: 'Equipment Hire' },
-    { name: 'Clothing Hire' }
+    { name: 'Specials' },
+    { name: 'Food' }
   ]);
 
   console.log('categories seeded');
@@ -18,52 +18,52 @@ db.once('open', async () => {
 
   const products = await Product.insertMany([
     {
-      name: 'Family Package 6 days',
+      name: 'Family Package 7 days',
       description:
-        'This package includes accomodation in the luxury rooms and meals.  Family are entitled to six hours unlimited equipment hire during duration of stay',
+        'This package includes accomodation in the luxury rooms and meals for the maximum of 6 persons including children.  Family are entitled to six hours unlimited equipment hire per day during the duration of stay.',
       image: 'imgfp6.jpg',
       category: categories[0]._id,
       price: 2500.00,
       quantity: 20
     },
     {
-      name: 'Canned Coffee',
+      name: 'Family Package 4 days',
       description:
-        'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      image: 'canned-coffee.jpg',
+        'This package includes accomodation in the luxury rooms and meals for the maximum of 6 persons including children. Family is entitled to six hours unlimited equipment hire per day during the duration of stay.',
+      image: 'imgfp4.jpg',
       category: categories[0]._id,
-      price: 1.99,
-      quantity: 500
+      price: 1500.00,
+      quantity: 10
     },
     {
-      name: 'Homemade Soy Candles',
+      name: 'Individual Package 7 days',
       category: categories[1]._id,
       description:
-        'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'candles.jpg',
-      price: 15.00,
-      quantity: 20
+        'This package includes accomodation in the luxury rooms and meals for the maximum of 2 persons, unlimited equipment hire during the duration of stay.',
+      image: 'imgid7.jpg',
+      price: 1200.00,
+      quantity: 10
     },
     {
-      name: 'Handmade Soap',
+      name: 'Individual Package 4 days',
       category: categories[1]._id,
       description:
-        'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      image: 'soap.jpg',
-      price: 3.99,
-      quantity: 50
+        'This package includes accomodation in the luxury rooms and meals for the maximum of 2 persons, unlimited equipment hire during the duration of stay.',
+      image: 'imgid4.jpg',
+      price: 800.00,
+      quantity: 10
     },
     {
-      name: 'Hamper',
-      category: categories[1]._id,
+      name: 'Group Package 7 days ',
+      category: categories[2]._id,
       description:
         'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      image: 'hamper.jpg',
-      price: 14.99,
-      quantity: 100
+      image: 'imgid7.jpg',
+      price: 3500.00,
+      quantity: 5
     },
     {
-      name: 'Camera',
+      name: 'Group Package 4 days',
       category: categories[2]._id,
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
@@ -72,8 +72,8 @@ db.once('open', async () => {
       quantity: 30
     },
     {
-      name: 'Tablet',
-      category: categories[2]._id,
+      name: 'Special Package',
+      category: categories[3]._id,
       description:
         'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
       image: 'tablet.jpg',
@@ -81,7 +81,7 @@ db.once('open', async () => {
       quantity: 30
     },
     {
-      name: 'Tales at Bedtime',
+      name: 'Transportation',
       category: categories[3]._id,
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
@@ -90,8 +90,8 @@ db.once('open', async () => {
       quantity: 100
     },
     {
-      name: 'Spinning Top',
-      category: categories[4]._id,
+      name: 'Transportation',
+      category: categories[3]._id,
       description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
       image: 'spinning-top.jpg',
       price: 1.99,
