baf4cdb1a5975b0488adb419609b964d1a34900b
Stella Ling <stellalph2@gmail.com>
2022-08-14 10:32:13 +0930

    to remove unwanted images and re-seeding

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -9,7 +9,7 @@ db.once('open', async () => {
     { name: 'Individual' },
     { name: 'Group' },
     { name: 'Specials' },
-    { name: 'Food' }
+    // { name: 'Food' }
   ]);
 
   console.log('categories seeded');
@@ -57,73 +57,39 @@ db.once('open', async () => {
       name: 'Group Package 7 days ',
       category: categories[2]._id,
       description:
-        'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      image: 'imgid7.jpg',
-      price: 3500.00,
+        'This package includes accomodation in the shared big apartment within the resort up to 10 persons with all the facilities including unlimited equipment hire that you would require for a comfortable stay throughout the period.  Date available (17 Aug - 24 Aug). Check-in time: 1:00 pm, Check-out time: 12:00 noon',
+      image: 'imggp7.jpg',
+      price: 4500.00,
       quantity: 5
     },
     {
       name: 'Group Package 4 days',
       category: categories[2]._id,
       description:
-        'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'camera.jpg',
-      price: 399.99,
-      quantity: 30
+        'This package includes accomodation in the shared big apartment within the resort up to 10 persons with all the facilities including unlimited equipment hire that you would require for a comfortable stay throughout the period. Date available (17 Aug - 24 Aug). Check-in time: 1:00pm, Check-out time: 12:00 noon',
+      image: 'imggp4.jpg',
+      price: 2600.00,
+      quantity: 5
     },
     {
-      name: 'Special Package',
+      name: 'Special Package 7 days',
       category: categories[3]._id,
       description:
         'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
       image: 'tablet.jpg',
-      price: 199.99,
-      quantity: 30
+      price: 2500.00,
+      quantity: 5
     },
     {
-      name: 'Transportation',
+      name: 'Special Package 4 days',
       category: categories[3]._id,
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
       image: 'bedtime-book.jpg',
-      price: 9.99,
-      quantity: 100
+      price: 900.00,
+      quantity: 5
     },
-    {
-      name: 'Transportation',
-      category: categories[3]._id,
-      description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      image: 'spinning-top.jpg',
-      price: 1.99,
-      quantity: 1000
-    },
-    {
-      name: 'Set of Plastic Horses',
-      category: categories[4]._id,
-      description:
-        'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-      image: 'plastic-horses.jpg',
-      price: 2.99,
-      quantity: 1000
-    },
-    {
-      name: 'Teddy Bear',
-      category: categories[4]._id,
-      description:
-        'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      image: 'teddy-bear.jpg',
-      price: 7.99,
-      quantity: 100
-    },
-    {
-      name: 'Alphabet Blocks',
-      category: categories[4]._id,
-      description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-      image: 'alphabet-blocks.jpg',
-      price: 9.99,
-      quantity: 600
-    }
+  
   ]);
 
   console.log('products seeded');
