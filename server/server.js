d50fcf6436fdf8a4ba8deee2306e8e3d0d22b0fc
Stella Ling <stellalph2@gmail.com>
2022-08-16 18:26:54 +0930

    deploying

--- server/server.js
+++ server/server.js
@@ -34,7 +34,11 @@ if (process.env.NODE_ENV === 'production') {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+}
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
