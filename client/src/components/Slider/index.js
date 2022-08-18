eb44a1dd12268b3ccd38d616d1c761a52943295e
Stella Ling <stellalph2@gmail.com>
2022-08-18 20:11:15 +0930

    modify screenshots and slider

--- client/src/components/Slider/index.js
+++ client/src/components/Slider/index.js
@@ -19,7 +19,7 @@ const items = [
     key: 1,
   },
   {
-    src: 'https://images.unsplash.com/photo-1482867996988-29ec3a0f1aac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    src: 'https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
     altText: 'Slide 2',
     caption: 'Slide 2',
     key: 2,
