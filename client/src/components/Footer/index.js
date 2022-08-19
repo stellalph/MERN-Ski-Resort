5fc71c08cc2ddda8751280506d26ceab482b6e03
Stella Ling <stellalph2@gmail.com>
2022-08-20 00:22:20 +0930

    fixing code in index.js and index.css

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -18,10 +18,10 @@ const Footer = () => {
                     {' '}by
                     {' '}<a className="footer px-2" href="https://github.com/stellalph" target="_blank" rel="noopener noreferrer">Stella Ling</a>
                 </span>
-                <span><a className="footer px-2" href="https://github.com/stellalph/MERN-Ski-Resort.git" target="_blank" rel="noopener noreferrer">< BsGithub style={{ color: 'white', fontSize: '37px' }} /></a></span>
-                <span><a className="footer px-2" href="https://www.linkedin.com/in/stella-ling-46246476" target="_blank" rel="noopener noreferrer">< BsLinkedin style={{ color: 'light-blue', fontSize: '37px' }} /></a></span>
-                <span><a className="footer px-2" href="https://twitter.com" target="_blank" rel="noopener noreferrer">< BsTwitter style={{ color: 'white', fontSize: '37px' }} /></a></span>
-                <span><a className="footer px-2" href="mailto:stella.ling@outlook.com" target="_blank" rel="noopener noreferrer">< BiMailSend style={{ color: 'light-blue', fontSize: '38px' }} /></a></span>
+                <span><a className="footer px-2" href="https://github.com/stellalph/MERN-Ski-Resort.git" target="_blank" rel="noopener noreferrer">< BsGithub style={{ color: 'white', fontSize: '30px' }} /></a></span>
+                <span><a className="footer px-2" href="https://www.linkedin.com/in/stella-ling-46246476" target="_blank" rel="noopener noreferrer">< BsLinkedin style={{ color: 'light-blue', fontSize: '30px' }} /></a></span>
+                <span><a className="footer px-2" href="https://twitter.com" target="_blank" rel="noopener noreferrer">< BsTwitter style={{ color: 'white', fontSize: '30px' }} /></a></span>
+                <span><a className="footer px-2" href="mailto:stella.ling@outlook.com" target="_blank" rel="noopener noreferrer">< BiMailSend style={{ color: 'light-blue', fontSize: '30px' }} /></a></span>
 
             </div>
 
