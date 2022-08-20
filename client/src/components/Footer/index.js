c3dede966f8f2db40ca8b390d688d4ce57a792e1
Stella Ling <stellalph2@gmail.com>
2022-08-20 21:10:55 +0930

    add code to footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -11,15 +11,15 @@ const Footer = () => {
 
         /* SOCIAL LINKS */
 
-        <footer className="footer mt-auto py-2 bg-dark">
+        <footer className="footer mt-auto py-3 bg-dark">
             <div className="flex-row space-between px-2">
-                <span className="footer px-3">
+                <span className="footer px-3"style={{color:'white'}}>
                     &copy;{(new Date().getFullYear())}
                     {' '}by
-                    {' '}<a className="footer px-2" href="https://github.com/stellalph" target="_blank" rel="noopener noreferrer">Stella Ling</a>
+                    {' '}<a className="footer px-2" href="https://github.com/stellalph" target="_blank" rel="noopener noreferrer" style={{ color:'white', fontSize:'20px'}}>Stella Ling</a>
                 </span>
                 <span><a className="footer px-2" href="https://github.com/stellalph/MERN-Ski-Resort.git" target="_blank" rel="noopener noreferrer">< BsGithub style={{ color: 'white', fontSize: '30px' }} /></a></span>
-                <span><a className="footer px-2" href="https://www.linkedin.com/in/stella-ling-46246476" target="_blank" rel="noopener noreferrer">< BsLinkedin style={{ color: 'light-blue', fontSize: '30px' }} /></a></span>
+                <span><a className="footer px-2" href="https://www.linkedin.com/in/stella-ling-46246476" target="_blank" rel="noopener noreferrer">< BsLinkedin style={{ color: 'white', fontSize: '30px' }} /></a></span>
                 <span><a className="footer px-2" href="https://twitter.com" target="_blank" rel="noopener noreferrer">< BsTwitter style={{ color: 'white', fontSize: '30px' }} /></a></span>
 
 
