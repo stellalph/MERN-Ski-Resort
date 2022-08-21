30e8cb91ceab5d76165f6c9412d0048d9f0b3339
Stella Ling <stellalph2@gmail.com>
2022-08-21 19:16:29 +0930

    add react-icon heart to Success.js

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -3,6 +3,7 @@ import { useMutation } from '@apollo/client';
 import Jumbotron from '../components/Jumbotron';
 import { ADD_ORDER } from '../utils/mutations';
 import { idbPromise } from '../utils/helpers';
+import { BsHeart } from '/react-icons/bs';
 
 function Success() {
   const [addOrder] = useMutation(ADD_ORDER);
@@ -32,7 +33,7 @@ function Success() {
   return (
     <div>
       <Jumbotron>
-        <h1>Success!</h1>
+        <h1>Success!<BsHeart style={{color:'red', fontSize:'30px'}}/></h1>
         <h2>Thank you for your purchase!</h2>
         <h2>You will now be redirected to the home page</h2>
       </Jumbotron>
